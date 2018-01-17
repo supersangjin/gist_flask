83fa304f5eddd6889ed495dfc67118090b92c981
sujin-jang <jsujin9603@gmail.com>
2018-01-17 17:26:29 +0900

    more responsive index

--- app/utils/views.py
+++ app/utils/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint, request, redirect, url_for, flash
-from app.models import Article, User
+from app.models import Article, User, Video
 from . import utils_blueprint
 from app import db
 from flask_login import login_required
@@ -9,7 +9,9 @@ ARTICLE_LIMIT = 4
 @utils_blueprint.route('/')
 def index():
 	articles = Article.query.limit(ARTICLE_LIMIT).all()
-	return render_template('utils/index.html', articles=articles)
+	videos = Video.query.limit(ARTICLE_LIMIT).all()
+
+	return render_template('utils/index.html', articles=articles, videos=videos)
 
 @utils_blueprint.route('/search')
 def search():
