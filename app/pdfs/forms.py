4f08e50b4e02a5d59972206f96f0144787242000
최상진 <hhclover@kaist.ac.kr>
2018-01-20 19:09:32 +0900

    google book api

--- app/pdfs/forms.py
+++ app/pdfs/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SelectField
 from wtforms.validators import DataRequired
 
 
 class UploadPdfForm(FlaskForm):
     pdf_title = StringField('Pdf Title', validators=[DataRequired()])
     pdf_description = StringField('Pdf Description', validators=[DataRequired()])
+    pdf_category = SelectField(
+        'Category',
+        choices=[(1, 'Fiction'), (2, 'Memoir'), (3, 'Science'), (4, 'History'), (5, 'Art')],
+        coerce=int
+    )
