b500360966789d540d3b91da958538a3107f0bc0
최상진 <hhclover@kaist.ac.kr>
2018-01-26 15:09:01 +0900

    book not found

--- app/books/googleBook.py
+++ app/books/googleBook.py
@@ -10,13 +10,13 @@ key1 = "AIzaSyCsTHxXG4ZlMqCEzc3MT8ZG_u48ehLUCxM"
 def search_isbn(isbn):
     r = requests.get("https://www.googleapis.com/books/v1/volumes?q=+isbn:" + isbn + "&key=" + key1)
     rj = r.json()
-    print(rj)
-    info = rj["items"][0]["volumeInfo"]
 
-    book = Book(isbn=isbn, title=info["title"], author=info["authors"][0], description=info["description"],
-                thumbnail=info["imageLinks"]["thumbnail"], category_id=1)
-
-    db.session.add(book)
-    db.session.commit()
-
+    if rj["totalItems"] == 0:
+        book = db.session.query(Book).filter(Book.id == 1).first()
+    else:
+        info = rj["items"][0]["volumeInfo"]
+        book = Book(isbn=isbn, title=info["title"], author=info["authors"][0], description=info["description"],
+                    thumbnail=info["imageLinks"]["thumbnail"], category_id=1)
+        db.session.add(book)
+        db.session.commit()
     return book
